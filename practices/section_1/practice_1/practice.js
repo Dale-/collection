0a447d2391544eb8aa1c95ceb957540aa82b7ecb
Dale- <18717394542@163.com>
2015-04-30 15:35:49 +0800

    Dale: finish section_1/practice_1

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,28 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+
+  var result = [];
+
+  for(var i = 0; i < collection_a.length; i++) {
+    if(exist(collection_a[i], collection_b)) {
+      result.push(collection_a[i]);
+    }
+  }
+
+  return result;
+}
+
+function exist(value, collection) {
+
+  var isExist = false;
+
+  for(var i = 0; i < collection.length; i++) {
+    if(value === collection[i]) {
+      isExist = true;
+      break;
+    }
+  }
+
+  return isExist;
 }
 
 module.exports = collect_same_elements;
